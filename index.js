2b52ae26d9349700cb724b3d4db2c4f70f97abe2
Sophia Barrett <sophia@yoursummit.media>
2021-11-28 17:27:31 -0700

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
-const Session = require('./lib/Session');
+const inquirer = require('inquirer');
+const Query = require('./lib/Query');
+const db = require('./db/connection');
+const cTable = require('console.table');
+const prompts = require('./utils/prompts');
 
-new Session().initializeApp();
+function initializeApp() {
+    promptUserForAction();
+};
+
+function promptUserForAction() {
+    return inquirer
+    .prompt(prompts.actions)
+    .then(({ action }) => {
+        if (action === 'View all departments') {
+            return new Query().viewDepartments();
+        }
+        if (action === 'View all roles') {
+            return new Query().viewRoles();
+        }
+        if (action === 'View all employees') {
+            return new Query().viewEmployees();
+        }
+        if (action === 'Add a department') {
+            return inquirer
+            // sql = insertDepartment;
+            // params.push()
+        }
+        if (action === 'Add a role') {
+
+        }
+        if (action === 'Add an employee') {
+
+        }
+        if (action === 'Update an employee role') {
+
+        } 
+    }).then(query => {
+        if (query.type === 'view') {
+            viewData(query);
+        }
+    });
+}
+
+function promptUserToContinue() {
+    return inquirer
+    .prompt(prompts.cont)
+    .then(({ cont }) => {
+        if (cont === 'EXIT') {
+            console.log('Bye!');
+            db.end();
+        } else {
+            promptUserForAction();
+        }
+    });
+}
+
+function viewData(query) {
+    db.promise()
+    .query(query.sql)
+    .then(response => {
+        console.log(``)
+        console.table(response[0]);
+    })
+    .then(() => promptUserToContinue());
+}
+
+initializeApp();
--- utils/prompts.js
+++ utils/prompts.js
@@ -1,29 +1,25 @@
-const actions = {
-    type: 'list',
-    name: 'action',
-    message: 'What would you like to do?',
-    choices: [
-        'View all departments',
-        'View all roles',
-        'View all employees',
-        'Add a department',
-        'Add a role',
-        'Add an employee',
-        'Update an employee role'
-    ]
-}
-
-const exit = {
-    type: 'list',
-    name: 'exit',
-    message: 'Would you like to continue?',
-    choices: [
-        'Continue',
-        'EXIT'
-    ]
-}
-
 module.exports = {
-    actions,
-    exit
-};
+    actions: {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ]
+    },
+    cont: {
+        type: 'list',
+        name: 'cont',
+        message: 'Would you like to continue?',
+        choices: [
+            'Continue',
+            'EXIT'
+        ]
+    }  
+}
